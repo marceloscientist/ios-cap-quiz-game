dcf3023394182c17ffa8dc80fda9295ca6ca584a
MARCELO DE CARVALHO SANTANA <marcelo.c.santana+bradesco@bradesco.com.br>
2026-04-26 14:13:20 -0300

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Santana, Marcelo de Carvalho on 26/04/26.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
